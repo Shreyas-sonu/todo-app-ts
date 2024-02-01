d59c3e7e029b9c60bc78bd609b42964324bf9d85
Shreyas <sonureigns007@gmail.com>
2024-02-01 18:14:15 +0530

    tsx project react app added

---
+++ src/App.tsx
@@ -0,0 +1,119 @@
+// import { useStorageState } from "./hooks/useStorageState";
+// import { defaultUser } from "./constants/defaultUser";
+// import { User } from "./types/user";
+// import { ColorPalette, GlobalStyles, MuiTheme } from "./styles";
+// import { ThemeProvider } from "@mui/material";
+// import { useEffect } from "react";
+// import { ErrorBoundary } from "./components";
+// import { MainLayout } from "./layouts/MainLayout";
+// import AppRouter from "./router";
+// import { Toaster } from "react-hot-toast";
+// import { useResponsiveDisplay } from "./hooks/useResponsiveDisplay";
+// import { UserContext } from "./contexts/UserContext";
+
+// function App() {
+//   const [user, setUser] = useStorageState<User>(defaultUser, "user");
+//   const isMobile = useResponsiveDisplay();
+
+//   // Initialize user properties if they are undefined
+//   // this allows to add new properties to the user object without error
+//   useEffect(() => {
+//     const updateNestedProperties = (userObject: any, defaultObject: any) => {
+//       if (!userObject) {
+//         return defaultObject;
+//       }
+
+//       Object.keys(defaultObject).forEach((key) => {
+//         const userValue = userObject[key];
+//         const defaultValue = defaultObject[key];
+
+//         if (typeof defaultValue === "object" && defaultValue !== null) {
+//           // If the property is an object, recursively update nested properties
+//           userObject[key] = updateNestedProperties(userValue, defaultValue);
+//         } else if (userValue === undefined) {
+//           // Update only if the property is missing in user
+//           userObject[key] = defaultValue;
+//         }
+//       });
+
+//       return userObject;
+//     };
+
+//     // Update user with default values for all properties, including nested ones
+//     setUser((prevUser) => {
+//       // Make sure not to update if user hasn't changed
+//       if (
+//         JSON.stringify(prevUser) !==
+//         JSON.stringify(updateNestedProperties({ ...prevUser }, defaultUser))
+//       ) {
+//         return updateNestedProperties({ ...prevUser }, defaultUser);
+//       }
+//       return prevUser;
+//     });
+//   }, []);
+
+//   return (
+//     <>
+//       <ThemeProvider theme={MuiTheme}>
+//         <GlobalStyles />
+//         <Toaster
+//           position="top-center"
+//           reverseOrder={false}
+//           gutter={12}
+//           containerStyle={{
+//             marginBottom: isMobile ? "96px" : "12px",
+//           }}
+//           toastOptions={{
+//             position: "bottom-center",
+//             duration: 4000,
+//             style: {
+//               padding: "14px 22px",
+//               borderRadius: "18px",
+//               fontSize: "17px",
+//               border: `2px solid ${ColorPalette.purple}`,
+//               background: "#141431e0",
+//               WebkitBackdropFilter: "blur(6px)",
+//               backdropFilter: "blur(6px)",
+//               color: ColorPalette.fontLight,
+//             },
+//             success: {
+//               iconTheme: {
+//                 primary: ColorPalette.purple,
+//                 secondary: "white",
+//               },
+//               style: {},
+//             },
+//             error: {
+//               iconTheme: {
+//                 primary: ColorPalette.red,
+//                 secondary: "white",
+//               },
+//               style: {
+//                 borderColor: ColorPalette.red,
+//               },
+//             },
+//           }}
+//         />
+//         <UserContext.Provider value={{ user, setUser }}>
+//           <ErrorBoundary>
+//             <MainLayout>
+//               <AppRouter />
+//             </MainLayout>
+//           </ErrorBoundary>
+//         </UserContext.Provider>
+//       </ThemeProvider>
+//     </>
+//   );
+// }
+
+// export default App;
+
+const App = () => {
+  return (
+    <div>
+      Hello todo
+    </div>
+  )
+}
+
+export default App
